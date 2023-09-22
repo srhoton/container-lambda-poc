fb906f12ad408a341abd50543cd30f35c1374df5
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-09-22 12:24:47 -0400

    Syntax error

--- application/provider.tf
+++ application/provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "sprhoto"
     workspaces {
-      prefix = "container-lambda-poc--"
+      prefix = "container-lambda-poc-"
     }
   }
   required_providers {
