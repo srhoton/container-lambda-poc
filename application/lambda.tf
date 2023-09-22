a5dc59f0f1b06a9933bfdf65e9f0a12cba00f449
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-09-22 12:47:16 -0400

    Force arch

--- application/lambda.tf
+++ application/lambda.tf
@@ -20,7 +20,8 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   package_type = "Image"
-  image_uri     = var.image_uri
+  image_uri = "874500785305.dkr.ecr.us-west-2.amazonaws.com/container-lambda-poc:initial-setup"
+  #image_uri     = var.image_uri
   role          = aws_iam_role.iam_for_lambda.arn
   function_name = "test_lambda"
   image_config {
